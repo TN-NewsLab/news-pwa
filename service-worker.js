3534e00e94669c05f21f7b86788bba27a4905365
TN-NewsLab <tomo_naka0616@icloud.com>
2025-10-27 15:58:19 +0900

    Service Workers fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,27 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = 'news-pwa-cache-v1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './news.json',
+];
+
+self.addEventListener('install', (event) => {
   console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching Files');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-self.addEventListener('fetch', e => {});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
